f11bf03863a09a9c173cca5ba21f214e4ac9c2aa
Alexander Danilovsky <alex.dllsky@gmail.com>
2021-02-26 16:45:54 +0300

    Новый класс для управления водой

--- Assets/Scripts/ConstantsAndEnums/InputEnum.cs
+++ Assets/Scripts/ConstantsAndEnums/InputEnum.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace ProjectDwarf.Enums
+﻿namespace ProjectDwarf.Enums
 {
     public enum EnumInputAdapters
     { 
